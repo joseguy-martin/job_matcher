712c94cecab86a55860d6abac54d78f356800b4e
Jose Martin <jgmm26@gmail.com>
2020-11-24 07:57:07 -0500

    * Adding comments to mangonel.py, removing extra lines and clarifying a variable. * Normalizing quotes for all files

--- lib/bulk_job_text.py
+++ lib/bulk_job_text.py
@@ -5,17 +5,17 @@ try:
     from selenium.webdriver.common.keys import Keys
     from selenium.webdriver.chrome.options import Options
 except (ModuleNotFoundError, ImportError):
-    with open(".install", 'w') as f:
+    with open('.install', 'w') as f:
         quit()
 
 chrome_options = Options()
-chrome_options.add_argument("--maximize-window")
+chrome_options.add_argument('--maximize-window')
 b = webdriver.Chrome(options=chrome_options)
 
 b.get('https://www.indeed.com/')
 sleep(2)
-b.find_element_by_xpath("/html/body/div/div[4]/div[1]/div/div/div/form/div[1]/div[1]/div/div[2]/input").send_keys("analyst")
-b.find_element_by_xpath("/html/body/div/div[4]/div[1]/div/div/div/form/div[3]/button").send_keys(Keys.ENTER)
+b.find_element_by_xpath('/html/body/div/div[4]/div[1]/div/div/div/form/div[1]/div[1]/div/div[2]/input').send_keys('analyst')
+b.find_element_by_xpath('/html/body/div/div[4]/div[1]/div/div/div/form/div[3]/button').send_keys(Keys.ENTER)
 
 with open('temp/all_text.txt', 'w') as all_text:
     for x in range (2):
@@ -25,7 +25,7 @@ with open('temp/all_text.txt', 'w') as all_text:
             try:
                 b.switch_to.frame('vjs-container-iframe')
             except exceptions.NoSuchFrameException:
-                print("Frame switch failed at element", i)
+                print('Frame switch failed at element', i)
                 quit(1)
             all_text.write(b.find_element_by_xpath('//div[@id="jobDescriptionText"]').text)
             b.switch_to.default_content()
