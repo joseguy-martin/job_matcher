cf99ffec0f47941fd6961874c6e996fa355295f0
Jose Martin <jgmm26@gmail.com>
2020-11-30 03:39:05 -0500

    Activating bulk_job_text, adding comments

--- lib/bulk_job_text.py
+++ lib/bulk_job_text.py
@@ -1,17 +1,18 @@
 try:
     import requests
     from bs4 import BeautifulSoup
-    from time import sleep
 except (ModuleNotFoundError, ImportError):
     with open('.install', 'w') as f:
         quit()
 
+# Get the start page and gather the links to each job
 start_page = requests.get('https://www.indeed.com/jobs?q=analyst&l=New%20York%2C%20NY&vjk=747467bd98c63be2')
 start_page_soup = BeautifulSoup(start_page.text, 'html.parser')
 job_links = ["https://www.indeed.com"+x.get('href') for x in start_page_soup.find_all('a', class_='jobtitle turnstileLink')]
 del start_page, start_page_soup
 
-with open('mangonel-cli/temp/all_text.txt', 'w') as all_text:
+# Write each job into the all-text file for scraping by common_lex
+with open('temp/all_text.txt', 'w') as all_text:
     for link in job_links:
         job_soup = BeautifulSoup(requests.get(link).text, 'html.parser')
         all_text.write(job_soup.get_text())
--- lib/common_lex.py
+++ lib/common_lex.py
@@ -6,21 +6,25 @@ except (ModuleNotFoundError, ImportError):
     with open('temp/.install', 'w') as f:
         quit()
 
+# Load spacy and read file.
 nlp = spacy.load('en_core_web_sm')
 doc = nlp(open('temp/all_text.txt', 'r').read())
 remove('temp/all_text.txt')
 del nlp
 
-verbs = [token.lemma_ for token in doc if token.is_stop != True and token.is_punct != True and token.pos_ == 'VERB']
+# Get verbs, filtering for stopwords, punctuation, auxiliaries
+verbs = [token.lemma_ for token in doc if not token.is_stop and token.pos_ == 'VERB' and token.tag_ != 'AUX']
 verb_export = [x[0] for x in Counter(verbs).most_common(100)]
 verb_export.sort()
 
+# Get all noun chunks
 noun_chunks = [chunk.text for chunk in doc.noun_chunks]
 noun_export = [x[0] for x in Counter(noun_chunks).most_common(100)]
 noun_export.sort()
 
 del verbs, doc
 
+# Write the results to the temp folder, handing off to mangonel.py
 with open('temp/common_verbs.txt', 'w') as f:
     f.writelines('\n'.join(verb_export))
 
