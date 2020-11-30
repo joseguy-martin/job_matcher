e9a0c3e26f58658c4846f37a4d6e1554f9e70842
Jose Martin <jgmm26@gmail.com>
2020-11-29 19:03:19 -0500

    Replacing Selenium with BeautifulSoup

--- lib/bulk_job_text.py
+++ lib/bulk_job_text.py
@@ -1,33 +1,19 @@
 try:
+    import requests
+    from bs4 import BeautifulSoup
     from time import sleep
-    from selenium import webdriver
-    from selenium.common import exceptions
-    from selenium.webdriver.common.keys import Keys
-    from selenium.webdriver.chrome.options import Options
 except (ModuleNotFoundError, ImportError):
     with open('.install', 'w') as f:
         quit()
 
-chrome_options = Options()
-chrome_options.add_argument('--maximize-window')
-b = webdriver.Chrome(options=chrome_options)
+start_page = requests.get('https://www.indeed.com/jobs?q=analyst&l=New%20York%2C%20NY&vjk=747467bd98c63be2')
+start_page_soup = BeautifulSoup(start_page.text, 'html.parser')
+job_links = ["https://www.indeed.com"+x.get('href') for x in start_page_soup.find_all('a', class_='jobtitle turnstileLink')]
+del start_page, start_page_soup
 
-b.get('https://www.indeed.com/')
-sleep(2)
-b.find_element_by_xpath('/html/body/div/div[4]/div[1]/div/div/div/form/div[1]/div[1]/div/div[2]/input').send_keys('analyst')
-b.find_element_by_xpath('/html/body/div/div[4]/div[1]/div/div/div/form/div[3]/button').send_keys(Keys.ENTER)
-
-with open('temp/all_text.txt', 'w') as all_text:
-    for x in range (2):
-        jobs = b.find_elements_by_xpath('//div[@class="jobsearch-SerpJobCard unifiedRow row result clickcard"]')
-        for i in range(len(jobs)):
-            jobs[i].click()
-            try:
-                b.switch_to.frame('vjs-container-iframe')
-            except exceptions.NoSuchFrameException:
-                print('Frame switch failed at element', i)
-                quit(1)
-            all_text.write(b.find_element_by_xpath('//div[@id="jobDescriptionText"]').text)
-            b.switch_to.default_content()
-b.close()
-del jobs
+with open('mangonel-cli/temp/all_text.txt', 'w') as all_text:
+    for link in job_links:
+        job_soup = BeautifulSoup(requests.get(link).text, 'html.parser')
+        all_text.write(job_soup.get_text())
+        del job_soup
+del job_links
